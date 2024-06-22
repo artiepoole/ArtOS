37af312263af6a3facd469e16b384a0ca44e58f1
ArtePoole <stuart.f.poole@gmail.com>
2024-06-22 20:23:31 +0100

    Establishing File Structure Adding blank CMakeLists

---
+++ src/ArtOS.cpp
@@ -0,0 +1,6 @@
+//
+// Created by artypoole on 22/06/24.
+//
+
+#include "ArtOS.h"
+
---
+++ src/ArtOS.h
@@ -0,0 +1,17 @@
+//
+// Created by artypoole on 22/06/24.
+//
+#include <string>
+
+#ifndef ARTOS_H
+#define ARTOS_H
+
+
+
+class ArtOS {
+
+};
+
+
+
+#endif //ARTOS_H
