e13305d51e7cbe26b0483353d4ab4b5a7d1bb456
ArtePoole <stuart.f.poole@gmail.com>
2024-07-02 16:11:52 +0100

    Separating ports.h for inb and outb

---
+++ include/ports.h
@@ -0,0 +1,32 @@
+//
+// Created by artypoole on 30/06/24.
+//
+
+#ifndef PORTS_H
+#define PORTS_H
+
+#include "types.h"
+
+extern "C"
+inline void outb(u16 port, u8 val)
+{
+    __asm__ volatile ( "outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+    /* There's an outb %al, $imm8 encoding, for compile-time constant port numbers that fit in 8b. (N constraint).
+     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
+     * The  outb  %al, %dx  encoding is the only option for all other cases.
+     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+}
+
+extern "C"
+inline u8 inb(u16 port)
+{
+    u8 ret;
+    __asm__ volatile ( "inb %w1, %b0"
+                   : "=a"(ret)
+                   : "Nd"(port)
+                   : "memory");
+    return ret;
+}
+
+
+#endif //PORTS_H
