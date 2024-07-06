63c6a8abcc73cf6e242f400f5914db62e5f60939
Jon Poole <jonfpoole@gmail.com>
2024-07-06 17:27:23 +0100

    Update commit with explination

--- test/string_test.cpp
+++ test/string_test.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
 
 namespace artosstd {
-    // Not sure what's going on but I can't seem to include this by its header file. This worked before this commit:
-    // https://github.com/stupoole/ArtOS/commit/11cb5bc220b64ac5915a5bf9120a50c374a906b2
+    // This has clashes with the stdlib from the host machine. We want to include this from a namespace but
+    // that doesn't work if you use the .h file. This is clearly grungy but I don't really have a better
+    // option, without putting our whole stdlib in a namespace.
     #include "../include/string.cpp"
 }
 
