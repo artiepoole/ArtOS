00cd462dceac1c3363914a91665722daac14eb64
Jon Poole <jonfpoole@gmail.com>
2024-07-06 16:58:10 +0100

    Not sure why but we can't include via the header file

--- test/string_test.cpp
+++ test/string_test.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 
 namespace artosstd {
-    #include "../include/string.h"
+    // Not sure what's going on but I can't seem to include this by its header file. This worked before this commit:
+    // https://github.com/stupoole/ArtOS/commit/11cb5bc220b64ac5915a5bf9120a50c374a906b2
+    #include "../include/string.cpp"
 }
 
 // Demonstrate some basic assertions.
