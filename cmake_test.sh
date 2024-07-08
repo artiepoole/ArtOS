317b2c56e84d81d95b110438e60af8ddd6aeafda
ArtePoole <stuart.f.poole@gmail.com>
2024-07-08 15:20:46 +0100

    commenting test script.

--- cmake_test.sh
+++ cmake_test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-
+# This is used by github to run the tests for Continuous Integration support.
 cmake -S test -B cmake-build-test && cmake --build cmake-build-test && (cd cmake-build-test && ctest)
