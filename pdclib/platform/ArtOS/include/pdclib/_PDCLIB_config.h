b31f1a2cec191a99b5ccdca5ced513b5f275cbca
ArtePoole <stuart.f.poole@gmail.com>
2024-07-19 23:01:31 +0100

    implemented a get_ticks function and a way to get the clock rate. In PDCLib the clock rate is specified as a macro, unfortunately, but I will use this for now and look to replace it later. Because QEMU is always using the default settings, this should not change.

--- pdclib/platform/ArtOS/include/pdclib/_PDCLIB_config.h
+++ pdclib/platform/ArtOS/include/pdclib/_PDCLIB_config.h
@@ -359,7 +359,7 @@ struct _PDCLIB_imaxdiv_t
 #else
 #define _PDCLIB_clock_t unsigned long long
 #endif
-#define _PDCLIB_CLOCKS_PER_SEC
+#define _PDCLIB_CLOCKS_PER_SEC 2000000000
 
 /* For time_t, no such divider exists. Most implementations use a count of    */
 /* seconds since a specified epoch. While PDCLib really should support other  */
