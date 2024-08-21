3eef3224c4ebcf6f1c423287427feca37c6248a4
ArtePoole <stuart.f.poole@gmail.com>
2024-08-21 17:19:28 +0100

    Input working, except it's dropping a lot of inputs.

--- doom/doomgeneric_artos.cpp
+++ doom/doomgeneric_artos.cpp
@@ -1,19 +1,66 @@
 #include <EventQueue.h>
+#include <logging.h>
+#include <ports.h>
 
+#include "doomgeneric/doomkeys.h"
 #include "../include/constants/types.h"
 
 extern "C" {
 #include "doomgeneric/doomgeneric.h"
 }
 
+
 #include "../src/sys/kernel.h"
 #include "../include/VideoGraphicsArray.h"
 
+
+u8 doom_key_map[128] =
+{
+    0, KEY_ESCAPE, '1', '2', '3', '4', '5', '6', '7', '8', /* 9 */
+    '9', '0', '-', '=', KEY_BACKSPACE, /* Backspace */
+    KEY_TAB, /* Tab */
+    'q', KEY_FIRE, KEY_USE, 'r', /* 19 */ // q w e r
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', KEY_ENTER, /* Enter key */
+    KEYB_CONTROL, /* 29   - Control */
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', /* 39 */
+    '\'', '`', KEY_RSHIFT, /* Left shift */
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n', /* 49 */
+    'm', ',', '.', '/', KEY_RSHIFT, /* Right shift */
+    '*',
+    KEY_LALT, /* Alt */
+    ' ', /* Space bar */
+    KEY_CAPSLOCK, /* Caps lock */
+    KEY_F1, /* 59 - F1 key ... > */
+    KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9,
+    KEY_F10, /* < ... F10 */
+    0, /* 69 - Num lock*/
+    0, /* Scroll Lock */
+    KEY_HOME, /* Home key */
+    KEY_UPARROW, /* Up Arrow */
+    0, /* Page Up */
+    KEY_MINUS,
+    KEY_LEFTARROW, /* Left Arrow */
+    0,
+    KEY_RIGHTARROW, /* Right Arrow */
+    KEYP_PLUS,
+    KEY_END, /* 79 - End key*/
+    KEY_DOWNARROW, /* Down Arrow */
+    KEY_PGDN, /* Page Down */
+    KEY_INS, /* Insert Key */
+    KEY_DEL, /* Delete Key */
+    0, 0, 0,
+    KEY_F11, /* F11 Key */
+    KEY_F12, /* F12 Key */
+    0, /* All other keys are undefined */
+};
+
 u32 screen_buffer[DOOMGENERIC_RESX * DOOMGENERIC_RESY];
 
+uint32_t start_ticks = 0;
 extern "C"
 void DG_Init()
 {
+    start_ticks = get_tick_ms();
 }
 
 extern "C"
@@ -26,6 +73,7 @@ extern "C"
 void DG_SleepMs(const uint32_t ms)
 {
     // fprintf("DG_SleepMs called: %d\n", ms);
+    LOG("Sleeping for ", ms, " ms");
     sleep_ms(ms);
 }
 
@@ -33,7 +81,7 @@ extern "C"
 uint32_t DG_GetTicksMs()
 {
     // fprintf("DG_GetTicksMs called: \n");
-    return get_tick_ms();
+    return get_tick_ms()-start_ticks;
 }
 
 extern "C"
@@ -42,23 +90,24 @@ int DG_GetKey(int* pressed, unsigned char* key)
     // should return 0 if no events
     // should update just one keypress if keypress
     // should skip to next event if not keypress
-    auto & queue = EventQueue::getInstance();
+    // TODO: Sending repeatedly held keys not working as intended. Keys not getting detected between frames
+    auto& queue = EventQueue::getInstance();
     if (!queue.pendingEvents()) return 0; // no events
 
     auto latest = queue.getEvent();
+
     switch (latest.type)
     {
-        case KEY_DOWN:
-            *pressed = 1;
-            *key = latest.data.lower_data;
-            break;
-        case KEY_UP:
-            *pressed = 0;
-            *key = latest.data.lower_data;
-            break;
+    case KEY_DOWN:
+        *pressed = 1;
+        *key = doom_key_map[latest.data.lower_data];
+        break;
+    case KEY_UP:
+        *pressed = 0;
+        *key = doom_key_map[latest.data.lower_data];
+        break;
     }
     return queue.pendingEvents();
-
 }
 
 extern "C"
